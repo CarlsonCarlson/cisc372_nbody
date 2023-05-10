925c9457c198ec777ba5bae061820eb11310e9b5
CarlsonCarlson <carlson.cheng888@gmail.com>
2023-05-10 17:00:04 -0400

    added stdio.h, cuda_arch for printf support

--- parallel_compute.cu
+++ parallel_compute.cu
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "vector.h"
 #include "config.h"
 #include <cuda_runtime.h>
@@ -6,6 +7,9 @@
 __global__ void make_accel_matrix(vector3** accels, vector3* values) {
 	int idx = blockIdx.x * blockDim.x + threadIdx.x;
 	accels[idx] = &values[idx*NUMENTITIES];
+	#if __CUDA_ARCH__ >= 200
+	printf("thread %d: %d\n", idx, accels[idx])
+	#endif
 }
 
 //compute: Updates the positions and locations of the objects in the system based on gravity.
